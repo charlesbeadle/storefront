502c4358bcae974c39ee91622c2b897e7a0ec057
Charles Beadle <charles.beadle@protonmail.com>
2023-04-25 22:00:15 -0400

    Add product cards

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../services/products.service'
-import { Product } from '../types/product'
+import { ProductService } from '../services/products.service';
+import { Product } from '../types/product';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
   products: Product[] = [];
   constructor(private productService: ProductService) {}
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(data => {
+    this.productService.getProducts().subscribe((data) => {
       this.products = data;
     });
   }
