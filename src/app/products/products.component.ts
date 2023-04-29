9aa31870be9c941167ed5b60df47ccfc2b93a9be
Charles Beadle <charles.beadle@protonmail.com>
2023-04-28 23:02:29 -0400

    products page additions

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { ProductService } from '../services/products.service';
 import { Product } from '../types/product';
+
+interface ProductQuantity {
+  [id: number]: number;
+}
 
 @Component({
   selector: 'app-products',
@@ -9,10 +13,34 @@ import { Product } from '../types/product';
 })
 export class ProductsComponent implements OnInit {
   products: Product[] = [];
+  productQuantity: ProductQuantity = {};
+  @Output() addProductsToCart = new EventEmitter<any>();
+  @Output() removeProductFromCart = new EventEmitter<any>();
+
+  addProduct(id: number): void {
+    this.productQuantity[id] += 1;
+  }
+
+  removeProduct(id: number): void {
+    if (this.productQuantity[id] === 0) {
+      this.productQuantity[id] = 0;
+    } else {
+      this.productQuantity[id] -= 1;
+    }
+  }
+
+  addToCart(): void {
+    this.addProductsToCart.emit(this.productQuantity);
+  }
+
   constructor(private productService: ProductService) {}
+
   ngOnInit(): void {
     this.productService.getProducts().subscribe((data) => {
       this.products = data;
+      this.products.forEach((product) => {
+        this.productQuantity[product.id] = 0;
+      });
     });
   }
 }
