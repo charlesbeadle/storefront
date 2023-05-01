ebc3c5c57da1c8a4892c91a4662b3b83c88e5b55
Charles Beadle <charles.beadle@protonmail.com>
2023-05-01 12:15:49 -0400

    Add links to product details

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [ProductListComponent],
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, RouterModule],
   exports: [ProductListComponent],
 })
 export class ProductModule {}
